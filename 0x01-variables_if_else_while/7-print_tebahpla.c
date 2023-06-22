4ac788d66c6098d449285f413b14ab25f99c6371
Snenhlanhla Mkhwanazi <snenhlanhlamkhwanazi24@gmail.com>
2023-06-22 04:06:08 -0700

    6-print_numberz

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,17 +4,4 @@
 
 /**
  * main - Entry point
- * Return: Always 0 (Success)
- */
-
-int main(void)
-
-{
-	int d;
-
-	for (d = '0'; d <= '9'; d++)
-	putchar(d);
-	putchar('\n');
-
-	return (0);
-}
+ * Return: Always 0 (Su
