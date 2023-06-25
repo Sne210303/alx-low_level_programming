5003de8dd076c1d4a3bbe7f3aa25b63825c229d8
Snenhlanhla Mkhwanazi <snenhlanhlamkhwanazi24@gmail.com>
2023-06-25 12:30:00 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,21 +15,19 @@ void print_diagonal(int n)
 	
 	 else
         {
-
-	       	int i, j;
-
-	 for (i = 0; i < n; i++)
-	 {
-	 for (j = 0; j < n; j++)
-	 {
-	 if (j == i
-	 _putchar('\\');
-	 else if (j < i)
-	 _putchar(' ');
-	 }
-	 _putchar('\n');
-	 }
-	 }
+      	int i, j;
+	for (i = 0; i < n; i++)
+       	{
+	for (j = 0; j < n; j++)
+	{
+      	if (j == i)
+     	_putchar('\\');
+	else if (j < i)
+	_putchar(' ');
+	}
+	_putchar('\n');
+	}
+	}
 }
 
 
