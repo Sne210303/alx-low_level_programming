891e753b93f869844ba2ac06cf2512dbc699f3ef
Snenhlanhla Mkhwanazi <snenhlanhlamkhwanazi24@gmail.com>
2023-06-27 00:31:36 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,19 +7,4 @@
  * Return: string
  */
 
-char *_strcpy(char *dest, char *src)
-{
-	int l = 0;
-	int x = 0;
-
-	while (*(scr + l) != '\0')
-	{
-		l++;
-	}
-	for (; x < l; x++)
-	{
-		dest[x] = src[x];
-	}
-	dest[l] = '\0';
-	return (dest);
-}	
+char *_strcpy
