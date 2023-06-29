3463692365747edc5c8de5c0070d52f62cf341e6
Snenhlanhla Mkhwanazi <snenhlanhlamkhwanazi24@gmail.com>
2023-06-28 23:41:15 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,20 +5,20 @@
  *
  * Return: Always 0.
  */
+
 void print_most_numbers(void)
 {
- int c;
+	int c;
 
- for (c = 48; c < 58; c++)
- {
- if (c != 50)
- {
- if (c != 52)
- {
- _putchar(c);
- }
- }
- }
- _putchar('\n');
+	for (c = 48; c < 58; c++)
+	{
+	if (c != 50)
+	{
+	if (c != 52)
+	{
+	_putchar(c);
+	}
+	}
+	}
+	_putchar('\n');
 }
-
